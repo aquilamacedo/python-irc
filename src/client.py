e1417f0d96fc09c1b14261d67841fefa2fd8f2a1
Aquila Macedo <aquilamacedo@riseup.net>
2023-01-10 03:29:30 -0300

    src: server: HELP and NICK implementation.
    This commit adds the implementation of the `/NICK` and `/HELP` commands.
    The implementation of the `/QUIT` command was also added, but it is still buggy.
    
    Signed-off-by: Aquila Macedo <aquilamacedo@riseup.net>

--- src/client.py
+++ src/client.py
@@ -1,14 +1,20 @@
 import socket
 import threading
 
-nickname = input()
-
-# global constants
+# global constants and variables
 host = 'localhost'
 port = 7777
-buffsz = 1024
+buffsz = 10240
 
-# global variables
+HELP_MESSAGE = """The list of commands available are:
+    /NICK - Give the user a nickname or change the previous one.
+    /USER - Specify a user's username, hostname, and real name.
+    /QUIT - log out of the client session.
+    /JOIN - Start listening to a specific channel.
+    /PART - Leave a specific channel.
+    /LIST - List all existing channels only on the local server.
+    /WHO - Query information about customers or channels.
+    /PRIVMSG – Send messages to users. The target can be a nickname or a channel. if the destination is a channel, the message must be broadcast to all users on the specified channel except message originator. If the target is a nickname, the message will be sent only to that user."""
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((host, port))
@@ -16,11 +22,15 @@ client.connect((host, port))
 def receiveMessages():
   while True:
     try:
-      message = client.recv(buffsz).decode('ascii')
-      if message == 'NICK':
-        client.send(nickname.encode('ascii'))
-      else:
-        print(message)
+      message = client.recv(buffsz).decode('utf-8')
+      # if message == 'QUIT':
+      #   stopThr = True
+      #   print("You have left the server!")
+      #   print("Press <Enter> to exit")
+      #   client.close()
+      # else:
+      print(message)
+
     except Exception as e:
       print("[EXCEPTION]", e)
       client.close()
@@ -28,11 +38,32 @@ def receiveMessages():
 
 def sendMessages():
   while True:
-    message = f'<{nickname}> {input("")}'
-    client.send(message.encode('ascii'))
+
+    message = input("")
+    target_name = message[6:]
+
+    print(f"> {message}")
+
+    if message.startswith('/'):
+        if message.startswith('/NICK'):
+          client.send(f"NICK {target_name}".encode('utf-8'))
+
+        elif message.startswith("/HELP"):
+          client.send(f"HELP {HELP_MESSAGE}".encode('utf-8'))
+
+        elif message.startswith("/QUIT"):
+          client.send(f"QUIT {target_name}".encode('utf-8'))
+    else:
+      client.send(message.encode('utf-8'))
+
+target_name = ''
 
 thread1 = threading.Thread(target=receiveMessages)
-thread2 = threading.Thread(target=sendMessages)
-
 thread1.start()
+
+thread2 = threading.Thread(target=sendMessages)
 thread2.start()
+#thread2 = threading.Thread(target=sendMessages, args=[target_name])
+
+#thread2.start()
+
--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ import socket
 # global contants
 host = 'localhost'
 port = 7777
-buffsz = 1024
+buffsz = 10240
 
 # global variables
 clients = list()
@@ -23,37 +23,54 @@ def main():
     client, address = socket.accept()
     print(f"[CONNECTION] {address} connected to the server")
 
-    client.send("NICK".encode('ascii'))
-
-    nickname = client.recv(buffsz).decode('ascii')
-    nicknames.append(nickname)
-    clients.append(client)
-
-    print(f"Nickname of the client is {nickname}!")
-    broadcast(f"{nickname} joined the chat!".encode('ascii'))
-    client.send("Connected to the server!".encode('ascii'))
-
     thread = threading.Thread(target=messagesTreatment, args=[client])
     thread.start()
-
 
 def messagesTreatment(client):
   while True:
     try:
-      message = client.recv(buffsz)
-      broadcast(message)
+      # bypass to conflict between broadcast and print
+      msg = message = client.recv(buffsz)
+
+      if msg.decode('utf-8').startswith("NICK"):
+        nickname = msg.decode('utf-8')[5:]
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}!")
+        broadcast(f"{nickname} joined the chat!".encode('utf-8'))
+        client.send("Connected to the server!".encode('utf-8'))
+
+      elif msg.decode('utf-8').startswith("HELP"):
+        help_message = msg.decode('utf-8')
+        client.send(f"{help_message[5:]}".encode('utf-8'))
+
+      elif msg.decode('utf-8').startswith("QUIT"):
+        nickname = msg.decode('utf-8')[5:]
+        quitUser(nickname)
+
+      else:
+        broadcast(message)
     except:
-      deleteUser(client)
-      break
+      if client in clients:
+        index = clients.index(client)
+        clients.remove(client)
+        client.close()
+        nickname = nicknames[index]
+        broadcast(f"{nickname} left the chat!".encode('utf-8'))
+        nicknames.remove(nickname)
+        break
 
-def deleteUser(client):
-  index = clients.index(client)
-  clients.remove(client)
-  client.close()
-  nickname = nicknames[index]
-  broadcast(f'{nickname} left the chat!'.encode('ascii'))
-  nicknames.remove(nickname)
-
+def quitUser(name):
+  if name in nicknames:
+    name_index = nicknames.index(name)
+    client_to_quit = clients[name_index]
+    clients.remove(client_to_quit)
+    client_to_quit.send("You have left the server!".encode('utf-8'))
+    client_to_quit.close()
+    nicknames.remove(name)
+    broadcast(f"{name} left the chat".encode('utf-8'))
 
 print("[!] Server is listening...")
 main()
